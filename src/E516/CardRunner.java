17c8bae0359705c12eb4636338a7fc58ac2822c5
Akib <kdakib13@gmail.com>
2023-08-13 00:03:22 +0600

    16th Problem

---
+++ src/E516/CardRunner.java
@@ -0,0 +1,17 @@
+package E516;
+
+import java.util.Scanner;
+
+public class CardRunner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the shorthand notation : ");
+        String notation = scanner.next();
+
+        Card card = new Card(notation);
+
+        String description =  card.getDescription();
+
+        System.out.println(description);
+    }
+}
