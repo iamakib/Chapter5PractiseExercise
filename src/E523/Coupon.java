78874099fee0823e552ee1e773ff4b1a57f35612
Akib <kdakib13@gmail.com>
2023-08-13 22:30:29 +0600

    23th Problem

---
+++ src/E523/Coupon.java
@@ -0,0 +1,32 @@
+package E523;
+
+import java.util.Scanner;
+
+public class Coupon {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the amount spent: $");
+        double amountSpent = scanner.nextDouble();
+        double couponPercentage = 0;
+
+        if (amountSpent < 10) {
+            couponPercentage = 0;
+        } else if (amountSpent >= 10 && amountSpent <= 60) {
+            couponPercentage = 0.08;
+        } else if (amountSpent > 60 && amountSpent <= 150) {
+            couponPercentage = 0.10;
+        } else if (amountSpent > 150 && amountSpent <= 210) {
+            couponPercentage = 0.12;
+        } else if (amountSpent > 210) {
+            couponPercentage = 0.14;
+        }
+
+        double couponAmount = amountSpent * couponPercentage;
+
+        System.out.println("Coupon Amount: $" + couponAmount);
+
+    }
+}
+
